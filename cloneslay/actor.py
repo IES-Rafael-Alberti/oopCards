e229121944b32a60aa15e74c96b503fcce62fdb9
Javier Ortega <fjortegan@gmail.com>
2021-04-19 10:16:41 +0200

    Actor & class diagram .png

--- cloneslay/actor.py
+++ cloneslay/actor.py
@@ -1,3 +1,20 @@
 class Actor:
-    def __init__(self, deck):
+    def __init__(self, deck, live_points=50, block_points=0):
         self.deck = deck
+        self.live_points = live_points
+        self.block_points = block_points
+        self.dead = False
+
+    def block(self, block):
+        self.block_points += block
+
+    def receive_attack(self, damage):
+        if self.block_points >= damage:
+            self.block_points -= damage
+        else:
+            damage -= self.block_points
+            self.block_points = 0
+            self.live_points -= damage
+            if self.live_points <= 0:
+                self.live_points = 0
+                self.dead = True
