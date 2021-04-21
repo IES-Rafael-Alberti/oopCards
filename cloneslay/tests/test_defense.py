3ec46d0a594f88ac7d77f1ebbf45386706c49c4d
César <80521851+Cesar-ctrl@users.noreply.github.com>
2021-04-21 13:05:16 +0200

    Add files via upload

---
+++ cloneslay/tests/test_defense.py
@@ -0,0 +1,16 @@
+from cloneslay.actor import Actor
+from cloneslay.cards.strike import Strike
+from cloneslay.cards.defense import Defense
+
+def test_strike():
+    my_defense = Defense()
+    my_strike = Strike()
+    actor1 = Actor([my_strike, my_defense])
+    actor2 = Actor([])
+    my_strike.activate(actor1, actor2)
+    assert actor2.live_points == 43
+    my_defense.activate(actor2)
+    assert actor2.block_points == 5
+    my_strike.activate(actor1, actor2)
+    assert actor2.block_points == 0
+    assert actor2.live_points == 41
