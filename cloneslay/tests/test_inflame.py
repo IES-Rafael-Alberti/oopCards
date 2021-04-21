d8350e94db3ffe02de4290757f8bee14f1c3dfe8
Jose Ramon <jrgaitero.alumno@iesrafaelalberti.es>
2021-04-21 12:16:24 +0200

    Added Card (Inflame)

---
+++ cloneslay/tests/test_inflame.py
@@ -0,0 +1,16 @@
+from cloneslay.actor import Actor
+from cloneslay.cards.inflame import Inflame
+from cloneslay.cards.strike import Strike
+
+
+def test_inflame():
+    my_inflame = Inflame()
+    my_strike = Strike()
+    actor1 = Actor([my_inflame, my_strike])
+    actor2 = Actor([])
+    my_strike.activate(actor1, actor2)
+    assert actor2.live_points == 43
+    my_inflame.activate(actor1, actor1)
+    assert actor1.strength == 2
+    my_strike.activate(actor1, actor2)
+    assert actor2.live_points == 34
