66a0c8312522fe039f1f05d11f14215857c8dbf3
César <80521851+Cesar-ctrl@users.noreply.github.com>
2021-04-23 18:59:13 +0200

    Borradisimo

--- cloneslay/cards/carnage.py
+++
@@ -1,9 +0,0 @@
-from cloneslay.card import Card
-
-
-class JoseJavier_Corrigiendo(Card):
-    def __init__(self):
-        super().__init__("Jose Javier Corrigiendo", 20, "attack", "JoseJavier te corrigió el examen recibe 20 de daño", None)
-
-    def activate(self, actor, goal):
-        Card.attack(20, actor, goal)
