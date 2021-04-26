487fa0cd0bdfd905931b4d6efcff0ed466737242
César <80521851+Cesar-ctrl@users.noreply.github.com>
2021-04-26 17:44:46 +0200

    Update carnage.py

--- cloneslay/cards/carnage.py
+++ cloneslay/cards/carnage.py
@@ -3,7 +3,7 @@ from cloneslay.card import Card
 
 class Carnage(Card):
     def __init__(self):
-        super().__init__("Carnage", 20, "attack", "Ethereal. Deal 20 damage.", None)
+        super().__init__("Carnage", 2, "attack", "Ethereal. Deal 20 damage.", None)
 
     def activate(self, actor, goal):
         Card.attack(20, actor, goal)
