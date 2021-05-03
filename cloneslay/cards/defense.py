6e4eaf54368251e3ff04095b2bf6e18a89bb929c
César <80521851+Cesar-ctrl@users.noreply.github.com>
2021-05-03 11:26:40 +0200

    Update defense.py

--- cloneslay/cards/defense.py
+++ cloneslay/cards/defense.py
@@ -8,4 +8,4 @@ class Defense (Card):
     # activate must have 2 arguments, you can make goal optional with default: goal=None
     def activate(self, actor, goal=None):
         Card.block(5, actor)
-        # Card.discard_used_card("Defend_card", actor)
+        # Card.discard_used_card("Defend", actor)
