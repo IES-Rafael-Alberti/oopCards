9d0f30821e11ff74ec8bd32fa8be7a7aa4a9e1b0
meta11co <fjortegan@gmail.com>
2021-05-04 20:06:41 +0200

    little adjustments to IronWave class

--- cloneslay/cards/ironwave.py
+++ cloneslay/cards/ironwave.py
@@ -1,9 +1,10 @@
 from cloneslay.card import Card
 
+
 class IronWave(Card):
     def __init__(self):
-        super().__init__("Iron Wave",1,"attack","Gain 5 block, deal 5 damage","")
+        super().__init__("Iron Wave", 1, "attack", "Gain 5 block.deal 5 damage", "")
 
     def activate(self, actor, goal):
-        Card.attack(5,actor,goal)
+        Card.attack(5, actor, goal)
         actor.block(self, 5)
