6fba00acbf58edf5a0c4587d0240d1af01cdab60
César <80521851+Cesar-ctrl@users.noreply.github.com>
2021-05-06 17:04:28 +0200

    Ajustados Sword boomerang, defense, inflame y strike

--- cloneslay/tests/test_defense.py
+++ cloneslay/tests/test_defense.py
@@ -2,15 +2,15 @@ from cloneslay.actor import Actor
 from cloneslay.cards.strike import Strike
 from cloneslay.cards.defense import Defense
 
-def test_strike():
+def test_defense():
     my_defense = Defense()
     my_strike = Strike()
     actor1 = Actor([my_strike, my_strike])
     actor2 = Actor([my_defense])
     my_strike.activate(actor1, actor2)
-    assert actor2.live_points == 43
+    assert actor2.live_points == 293
     my_defense.activate(actor2)
     assert actor2.block_points == 5
     my_strike.activate(actor1, actor2)
     assert actor2.block_points == 0
-    assert actor2.live_points == 41
+    assert actor2.live_points == 291
