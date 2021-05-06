47bf22635599d2a3bb033946baa093ff52059792
César <80521851+Cesar-ctrl@users.noreply.github.com>
2021-05-06 11:53:56 +0200

    Update test_carnage.py

--- cloneslay/tests/test_carnage.py
+++ cloneslay/tests/test_carnage.py
@@ -2,15 +2,15 @@ from cloneslay.actor import Actor
 from cloneslay.cards.carnage import Carnage
 from cloneslay.cards.defense import Defense
 
-def test_strike():
+def test_carnage():
     my_defense = Defense()
     my_carnage = Carnage()
     actor1 = Actor([my_carnage, my_carnage])
     actor2 = Actor([my_defense])
     my_carnage.activate(actor1, actor2)
-    assert actor2.live_points == 30
+    assert actor2.live_points == 280
     my_defense.activate(actor2)
     assert actor2.block_points == 5
     my_carnage.activate(actor1, actor2)
     assert actor2.block_points == 0
-    assert actor2.live_points == 15
+    assert actor2.live_points == 265
