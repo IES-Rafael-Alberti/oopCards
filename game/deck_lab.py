613ab964a97e88a60486080a1e07777a77faca6b
mora <jjmora.alumno@iesrafaelalberti.es>
2021-05-06 18:50:44 +0200

    linting and deck_lab started

--- game/deck_lab.py
+++ game/deck_lab.py
@@ -12,17 +12,36 @@ from pygame.transform import scale
 from cloneslay.actor import Actor
 from cloneslay.card import Card
 from game.button import Button
+from game.game import Game
 from game.character import Character
 from game.displayed_card import DisplayedCard
 
 class DeckLab:
     def __init__(self):
         self.deck_lab_scene()
-        self.started_cards
-        self.common_cards
-        self.uncommon_cards
-        self.rare_cards
+        self.started_cards = json.load(open("../assets/configuration_files/started_cards.json"))
+        self.common_cards = json.load(open("../assets/configuration_files/common_cards.json"))
+        self.uncommon_cards = json.load(open("../assets/configuration_files/uncommon_cards.json"))
+        self.rare_cards = json.load(open("../assets/configuration_files/rare_cards.json"))
+        self.group_cards = self.separate_cards(self.common_cards)
+        self.showed_cards
+        self.new_deck
         self.configuration_file = json.load(open("../assets/configuration_files/deck_lab_configuration.json"))
+
+
+        def separate_cards(self, deck):
+            if deck.size()<=15:
+                return  deck
+            counter = 0
+            deck = []
+            deck_part=[]
+            for card in deck:
+                counter +=1
+                deck_part.append(card)
+                if counter == 15:
+                    deck.append(deck_part)
+                    deck_part=[]
+            return deck
 
         def deck_lab_scene(self):
             pygame.init()
@@ -36,7 +55,8 @@ class DeckLab:
             self.cursor = scale(pygame.image.load("assets/cursor/cursor.png"), (25, 25))
             self.left_button = Button(scale(pygame.image.load("assets/left_button.png"), (250, 150)),Vector2(1344, 350),"")
             self.right_button = Button(scale(pygame.image.load("assets/right_button.png"), (250, 150)),Vector2(100, 350), "")
-            self.save_deck = Button(scale(pygame.image.load("assets/save_deck_name.png"), (250, 150)),Vector2(100, 350), "")
+            self.save_deck_name_button = Button(scale(pygame.image.load("assets/save_deck_name.png"), (250, 150)),Vector2(622, 70), "Save your Deck")
+            Game.print_text(self.screen,"Cards: "+self.new_deck.size()+"/")
 
         def handle_input(self):
             for event in pygame.event.get():
@@ -49,18 +69,33 @@ class DeckLab:
                 self._handle_mouse_over()
 
         def _handle_mouse_up(self):
-            # end turn button
-            if self.end_turn_button.rect.collidepoint(pygame.mouse.get_pos()):
-                self._end_turn()
-            # cards in hand
-            for card in self.active_card_deck:
-                if not card.card.used and card.rect and card.rect.collidepoint(pygame.mouse.get_pos()) \
-                        and card.card.energy <= self.active_actor.actor.energy:
-                    card.card.use(self.active_actor.actor, self.get_enemy())
-                    card.active = False
-                    self.active_actor.actor.energy -= card.card.energy
+            if self.left_button.rect.collidepoint(pygame.mouse.get_pos()):
+                if self.showed_cards == self.group_cards[0]:
+                    pass
+                else:
+                    self.showed_cards = self.group_cards[self.group_cards.find(self.showed_cards)-1]
+            if self.right_button.rect.collidepoint(pygame.mouse.get_pos()):
+                if self.showed_cards == self.group_cards[enumerate(self.showed_cards-1)]:
+                    pass
+                else:
+                    self.showed_cards = self.group_cards[self.group_cards.find(self.showed_cards)+1]
+            if self.save_deck_name_button.rect.collidepoint(pygame.mouse.get_pos()):
+
+
+        def draw_scene(self):
+            self.screen.blit(self.background, (0, 0))
+            if self.showed_cards:
+                initial_position = int(1920 / 2 - 250 * len(self.showed_cards) / 2)
+                counter = 0
+                for i, card in enumerate(self.showed_cards):
+                    counter+=1
+                    card.draw(self.screen, Vector2(initial_position + 250 * i, 600))
+            self.end_turn_button.draw(self.screen)
+            self.screen.blit(self.cursor, pygame.mouse.get_pos())
+            pygame.display.flip()
 
         def _handle_mouse_over(self):
             for card in self.active_card_deck:
                 if not card.card.used and card.rect:
                     card.active = card.rect.collidepoint(pygame.mouse.get_pos())
+
