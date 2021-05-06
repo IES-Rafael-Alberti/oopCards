266eb81d66fc6cf0d97b0193a7b9a63dc6671dbe
César <80521851+Cesar-ctrl@users.noreply.github.com>
2021-05-06 19:20:43 +0200

    He cambiado test_get_normal, test_get_short, test_exhaust_cards y he eliminado test_discard_cards. Los otros dos test dan error pero como no se muy bien como funcionan mejor que lo haga otro

--- cloneslay/tests/test_deck.py
+++ cloneslay/tests/test_deck.py
@@ -10,24 +10,31 @@ import os
 def test_get_normal():
     my_deck = Deck([Strike() for _ in range(22)])
     my_actor = Actor(my_deck)
-    my_actor.get_cards()
+    my_actor.init_turn()
     assert my_actor.draw.size() == 17
     assert my_actor.hand.size() == 5
     assert my_actor.discarded.size() == 0
     assert my_actor.exhausted.size() == 0
-    my_actor.get_cards()
+    my_actor.end_turn()
+    assert my_actor.draw.size() == 17
+    assert my_actor.hand.size() == 0
+    assert my_actor.discarded.size() == 5
+    my_actor.init_turn()
     assert my_actor.draw.size() == 12
     assert my_actor.hand.size() == 5
     assert my_actor.discarded.size() == 5
-    my_actor.get_cards()
-    assert my_actor.draw.size() == 7
-    assert my_actor.hand.size() == 5
+    my_actor.end_turn()
+    assert my_actor.draw.size() == 12
+    assert my_actor.hand.size() == 0
     assert my_actor.discarded.size() == 10
-    my_actor.get_cards()
+    my_actor.init_turn()
+    my_actor.end_turn()
+    my_actor.init_turn()
+    my_actor.end_turn()
     assert my_actor.draw.size() == 2
-    assert my_actor.hand.size() == 5
-    assert my_actor.discarded.size() == 15
-    my_actor.get_cards()
+    assert my_actor.hand.size() == 0
+    assert my_actor.discarded.size() == 20
+    my_actor.init_turn()
     assert my_actor.draw.size() == 17
     assert my_actor.hand.size() == 5
     assert my_actor.discarded.size() == 0
@@ -36,6 +43,7 @@ def test_get_normal():
 def test_get_short():
     my_deck = Deck([Strike() for _ in range(4)])
     my_actor = Actor(my_deck)
+    my_actor.init_turn()  
     assert my_actor.draw.size() == 0
     assert my_actor.hand.size() == 4
     assert my_actor.discarded.size() == 0
@@ -44,39 +52,29 @@ def test_get_short():
     assert my_actor.hand.size() == 4
     assert my_actor.discarded.size() == 0
 
-def test_discard_cards():
-    my_strike = Strike()
-    my_deck = Deck([my_strike for _ in range(22)])
-    my_actor1 = Actor(my_deck)
-    my_actor2 = Actor([])
-    assert my_actor1.draw.size() == 17
-    assert my_actor1.hand.size() == 5
-    assert my_actor1.discarded.size() == 0
-    my_strike.activate(my_actor1, my_actor2)
-    assert my_actor1.draw.size() == 17
-    assert my_actor1.hand.size() == 4
-    assert my_actor1.discarded.size() == 1
 
 def test_exhaust_cards():
-    my_strike = Strike()
-    my_carnage = Carnage()
-    my_deck = Deck([my_strike, my_carnage, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike, my_strike])
+    my_deck = Deck([Strike() for _ in range(4)])
+    my_deck.add_card(Carnage())
     my_actor1 = Actor(my_deck)
     my_actor2 = Actor([])
-    assert my_actor1.draw.size() == 17
+    my_actor1.init_turn()
+    assert my_actor1.draw.size() == 0
     assert my_actor1.hand.size() == 5
     assert my_actor1.discarded.size() == 0
     assert my_actor1.exhausted.size() == 0
-    my_strike.activate(my_actor1, my_actor2)
-    assert my_actor1.draw.size() == 17
-    assert my_actor1.hand.size() == 4
-    assert my_actor1.discarded.size() == 1
+    (my_deck.get_card_byname("Strike")).use(my_actor1, my_actor2)
+    assert my_actor1.draw.size() == 0
+    assert my_actor1.hand.size() == 5
+    assert my_actor1.discarded.size() == 0
     assert my_actor1.exhausted.size() == 0
-    my_carnage.activate(my_actor1, my_actor2)
-    assert my_actor1.draw.size() == 17
-    assert my_actor1.hand.size() == 3
-    assert my_actor1.discarded.size() == 1
+    (my_deck.get_card_byname("Carnage")).use(my_actor1, my_actor2)
+    my_actor1.end_turn()
+    assert my_actor1.draw.size() == 0
+    assert my_actor1.hand.size() == 0
+    assert my_actor1.discarded.size() == 4
     assert my_actor1.exhausted.size() == 1
+    
 
 
 def test_save_deck():
